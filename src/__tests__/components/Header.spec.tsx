1e571e684ca3f98237c14e01ee9a0cee4ba82d16
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2021-03-25 08:38:19 -0300

    Improve Header component tests

--- src/__tests__/components/Header.spec.tsx
+++ src/__tests__/components/Header.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
 import Header from '../../components/Header';
+import { useCart } from '../../hooks/useCart';
 
 jest.mock('react-router-dom', () => {
   return {
@@ -10,7 +11,7 @@ jest.mock('react-router-dom', () => {
 
 jest.mock('../../hooks/useCart', () => {
   return {
-    useCart: () => ({
+    useCart: jest.fn(() => ({
       cart: [
         {
           amount: 2,
@@ -29,15 +30,44 @@ jest.mock('../../hooks/useCart', () => {
           title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
         },
       ],
-    }),
+    })),
   };
 });
 
+const useCartMock = useCart as jest.Mock;
+
 describe('Header Component', () => {
   it('should be able to render the amount of products added to cart', () => {
-    const { getByTestId } = render(<Header />);
+    const { getByLabelText } = render(<Header />);
 
-    const cartSizeCounter = getByTestId('cart-size');
+    const cartSizeCounter = getByLabelText('cart-size');
     expect(cartSizeCounter).toHaveTextContent('2 itens');
+  });
+
+  it('should be able to render no data message when cart is empty', () => {
+    useCartMock.mockImplementation(() => ({ cart: [] }));
+
+    const { getByLabelText } = render(<Header />);
+
+    const cartSizeCounter = getByLabelText('cart-size');
+    expect(cartSizeCounter).toHaveTextContent('Sem itens');
+  });
+
+  it('should be able to render cart message when it has just one item', () => {
+    useCartMock.mockImplementation(() => ({
+      cart: [{
+        amount: 2,
+        id: 1,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+        price: 179.9,
+        title: 'Tênis de Caminhada Leve Confortável',
+      },]
+    }));
+
+    const { getByLabelText } = render(<Header />);
+
+    const cartSizeCounter = getByLabelText('cart-size');
+    expect(cartSizeCounter).toHaveTextContent('1 item');
   });
 });
